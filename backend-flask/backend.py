eb198ba10a1d2158b280d14be16acf77f4259ba1
ibsina <ibsina@gmail.com>
2023-01-17 23:17:02 +0800

    aa

---
+++ backend-flask/backend.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify,render_template,redirect, url_for
+import requests
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST']='172.22.0.2'
+app.config['MYSQL_USER']='root'
+app.config["MYSQL_PASSWORD"]='123'
+app.config["MYSQL_DB"]='flaskapp'
+
+mysql=MySQL(app)
+CORS(app)
+
+@app.route("/registration",methods=['GET','POST'])
+def registration():
+	cur=mysql.connection.cursor()
+	data=request.get_json()
+	print("JSON:")
+	print(data)
+	firstname = data['firstname']
+	lastname = data['lastname']
+	email = data['email']
+	username = data['username']
+	password = data['password']
+	print(firstname,lastname,email,username,password)
+	query="INSERT INTO users VALUES('{}','{}','{}','{}','{}')".format(firstname,lastname,email,username,password)
+	print(query)
+	cur.execute(query)
+	cur.execute("SELECT * FROM users")
+	fetchdata=cur.fetchall()
+	print(fetchdata)
+	mysql.connection.commit()
+	cur.close()
+	return data
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+	cur=mysql.connection.cursor()
+	data=request.get_json()
+	username = data['username']
+	password = data['password']
+	query="select username from users where username = '" + username + "' and password = '" + password + "';"
+	cur.execute(query)
+	row = cur.fetchone()
+	mysql.connection.commit()
+	cur.close()
+	if row is not None:
+		return jsonify(result="true")
+	else:
+		return jsonify(result="false")
+
+if __name__ == "__main__":
+    app.run(debug= True,port=5000,host='0.0.0.0',ssl_context=('cert.pem', 'key.pem'))
---
+++ frontend-html/front.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify,render_template,redirect, jsonify
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app)
+here=""
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route("/addrec", methods=['GET','POST'])
+def addrec():
+	if request.method == 'POST':
+		firstname = request.form['firstname']
+		lastname = request.form['lastname']
+		email = request.form['email']
+		username = request.form['username']
+		password = request.form['password']
+		here=request.form
+		requests.post('https://172.22.0.3:5000/registration',json=dict(here),verify=False)
+	return render_template('thanks.html')
+
+@app.route("/chkrec", methods=['GET','POST'])
+def chkrec():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		here=request.form
+		response = requests.get('https://172.22.0.3:5000/login',json=dict(here),verify=False)
+		response_dict = json.loads(response.text)
+		result = response_dict["result"]
+
+	if result == "true":
+		return render_template('success.html')
+	else:
+		return render_template('fail.html')
+
+if __name__ == "__main__":
+    app.run(debug= True,port=5001,host="0.0.0.0",ssl_context=('cert.pem', 'key.pem'))
