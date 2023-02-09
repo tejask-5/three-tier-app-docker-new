b929437da51a75f02c6d2633cf2f7f4377a1f79d
ibsina <ibsina@gmail.com>
2023-02-09 10:25:56 +0800

    update with fwb owner and new frontend

---
+++ frontend-direct-html/front.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify,render_template,redirect, jsonify
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app)
+here=""
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route("/addrec", methods=['GET','POST'])
+def addrec():
+	if request.method == 'POST':
+		firstname = request.form['firstname']
+		lastname = request.form['lastname']
+		email = request.form['email']
+		username = request.form['username']
+		password = request.form['password']
+		here=request.form
+		requests.post('http://seahk-is-fwb-internal-5cf4808a9027dc93.elb.ap-southeast-1.amazonaws.com/registration',json=dict(here))
+	return render_template('thanks.html')
+
+@app.route("/chkrec", methods=['GET','POST'])
+def chkrec():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		here=request.form
+		response = requests.get('http://seahk-is-fwb-internal-5cf4808a9027dc93.elb.ap-southeast-1.amazonaws.com/login',json=dict(here),verify=False)
+		response_dict = json.loads(response.text)
+		result = response_dict["result"]
+
+	if result == "true":
+		return render_template('success.html')
+	else:
+		return render_template('fail.html')
+
+if __name__ == "__main__":
+	app.run(debug= True,port=5002,host="0.0.0.0")
